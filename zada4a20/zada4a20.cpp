975e914eccf3fe50b7cf9d53e20753fb8618fdaf
livevil <55311226+l1veevil@users.noreply.github.com>
2020-01-13 09:50:35 +0300

    Add files via upload

---
+++ zada4a20/zada4a20.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+void display(int* arr, int size) {
+    for (int i = 0; i < size; i++) cout << arr[i] << " ";
+    delete[] arr;
+}
+
+void babl(int size, int* arr) {
+    int t;
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                t = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = t;
+            }
+        }
+    }
+}
+
+int main(){
+    int size;
+    cin >> size;
+    int* arr = new int[size];
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+    babl(size,arr);
+    display(arr, size);
+    /*int* arr = new int[size];
+
+   
+        for (int i = 0; i < size; i++) {
+            cin >> arr[i];
+        }
+    
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    t = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = t;
+                }
+            }
+        }
+        
+       for (int i = 0; i < size; i++) {
+            cout << arr[i] << " ";
+        }
+        delete[] arr;*/
+}
+
