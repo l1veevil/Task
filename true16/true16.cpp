248a64434a1f350c2c561347c2dc142624a275c3
livevil <55311226+l1veevil@users.noreply.github.com>
2020-01-13 10:06:09 +0300

    Add files via upload

---
+++ true16/true16.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void factorization(int n) {
+    int p = 2;
+    cout << n << " = 1";
+    while (n > 1) {
+        while (n % p == 0)
+        {
+            n /= p;
+            cout << " * " << p;
+        }
+        if (p == 2) p++;
+        else p += 2;
+    }
+    cout << "\nЕдиницу можно отбросить" << endl;
+}
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+    int x;
+    cout << "Введите число: ";
+    cin >> x;
+    factorization(x);
+}
---
+++ zada4a19/zada4a19.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int arr[10]{};
+	int size = 0;
+	int inp = 0;
+	cin >> size;
+	for (int i = 0; i < size; i++) { 
+		cin >> inp;
+		arr[inp]++;
+	}
+	for (int i = 0; i < 10; i++) if (arr[i]) printf("%i: %i \n", i, arr[i]);
+
+}
