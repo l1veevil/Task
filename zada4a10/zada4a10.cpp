2a8f134c9362e8304cd978c6b0e85a3caeb0f2a5
livevil <55311226+l1veevil@users.noreply.github.com>
2020-01-13 09:56:18 +0300

    Add files via upload

---
+++ zada4a10/zada4a10.cpp
@@ -0,0 +1,50 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	setlocale(LC_ALL, "Russian");
+	
+	double n, d, f;
+
+	cin >> n >> d; // n число которое надо возвести в степень, d степень
+
+	f = n;
+	if (d == 0) {
+		f = 1;
+		if (f == 0);
+		cout << f;
+	}
+	else {
+		if (n == 0 and d < 0) {
+			cout << "error";
+		}
+		else {
+			if (d > 0) {
+
+				while (d > 1) {
+					f *= n;
+					d--;
+
+				}
+				cout << f;
+			}
+			else{
+				while (d < -1) {
+					f *= n;
+					d++;
+				}
+				f = 1 / f;
+				cout << f;
+			}
+			if (f == 0) {
+				
+				
+			}
+
+		}
+		
+	}
+	
+}
---
+++ zada4a9/zada4a9.cpp
@@ -0,0 +1,52 @@
+﻿
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	srand(time(0));    
+	int i, a, n, ran, c, z;
+	i = 1;
+	do {
+		cout << "i zagadal chislo" << endl;
+		ran = rand() % 100;
+
+		while (i <= 5) {
+			cout << "RAN = " << ran << '\n';
+			cin >> a;
+
+			if (a == ran) {
+				cout << "you win" << endl;
+				break;
+			}
+			else {
+				if (i == 5) {
+					cout << "You lose! Загаданное число было: " << ran << endl;
+					break;
+				}	
+				if (a < ran) {
+					cout << "Загаданное число больше" << endl;
+
+				}
+				else {
+					cout << "Загаданное число меньше" << endl;
+				}
+				
+			}
+
+			i++;
+		}
+		
+		cout << "press 1 to restart" << endl;
+		cin >> z;
+		i = 1;
+		if (z != 1) break;
+	} while (true);
+		
+
+	
+	
+	
+}
