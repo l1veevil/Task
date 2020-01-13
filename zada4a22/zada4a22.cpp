d4d1a0f2776ade1b133684337bf92cf4ece9ca7a
livevil <55311226+l1veevil@users.noreply.github.com>
2020-01-13 10:07:49 +0300

    Add files via upload

---
+++ zada4a22/zada4a22.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void swap(int*a, int*b) {
+    int t;
+    t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int msort(int* a, int* b, int* c) {
+    if (*a > *b and *a > *c) return *a;
+    else if (*b > *a and *b > *c) return *b;
+    else if (*c > *b and *c > *a)return *c;
+}
+
+void info(const int* a, const int* b, const int* c) {
+    cout << "Адрес: " << a << " " << "Значение: " << *a << endl;
+    cout << "Адрес: " << b << " " << "Значение: " << *b << endl;
+    cout << "Адрес: " << c << " " << "Значение: " << *c << endl;
+}
+
+int* add(int* a, const int* с) {
+    *a += *с;
+    return a;
+}
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+    int a = 1;
+    int b = 2;
+    int c = 3;
+
+    
+    info(&a, &b, &c);
+    swap(&a, &b);
+    cout << "Максимум: " << msort(&a, &b, &c) << endl;
+    cout << add(&a, &c) << " " << *add(&a, &c);
+   
+}
+
---
+++ zada4a23/zada4a23.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+bool polindrom(string n) {
+    int len = n.length();
+
+    for (int i = 0; i < len / 2; ++i) {
+        if (n[i] != n[len - i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    string n;
+    cout << "Vvedite slovo" << endl;
+    cin >> n;
+    if (polindrom(n)) {
+        cout << "Polindrom";
+    }
+    else {
+        cout << "Not polindrom";
+    }
+}
+
