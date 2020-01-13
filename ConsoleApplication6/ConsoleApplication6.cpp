e87783df7d099d2c1c83edcaf31ab108b762f7cd
livevil <55311226+l1veevil@users.noreply.github.com>
2020-01-13 09:52:49 +0300

    Add files via upload

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,60 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main(){
+	setlocale(LC_ALL, "Russian");
+   
+	float a, b, c, D, x1, x2, x;
+	
+	cout << "Введите а" << endl;
+	cin >> a;
+
+	cout << "Введите b" << endl;
+	cin >> b;
+
+	cout << "Введите с" << endl;
+	cin >> c;
+
+
+	
+	if (a == 0) {
+		if (b == 0) {
+			if (c == 0) {
+				cout << "бесконечное множество точек пересечения" << endl;
+			}
+			else {
+				cout << "точек пересечения нет" << endl;
+			}
+			
+		}
+		else {
+			x = -c / b;
+			cout << x << endl;
+
+		}
+	}
+	else {
+		D = b * b - 4 * a * c;
+		if (D < 0){
+			cout << "Корней нет" << endl;
+		}
+		else 
+			if (D == 0) {
+				x = -b / (2 * a);
+				cout << x << endl;
+			}else
+		
+			{
+			
+				x1 = (-b + sqrt(D)) / (2 * a);
+				x2 = (-b - sqrt(D)) / (2 * a);
+
+				cout << x1 << endl;
+				cout << x2 << endl;
+			}
+	}
+	
+	
+}
+
