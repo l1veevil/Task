c9802cec9fd88aa68e83526a683332a9e4a19dce
livevil <55311226+l1veevil@users.noreply.github.com>
2020-01-13 09:46:04 +0300

    Add files via upload

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	setlocale(LC_ALL, "Russian");
+
+	int a , b;
+	cout << "Введите число" << endl;
+	cin >> a;
+	
+	cout << "Введите число" << endl;
+	cin >> b;
+
+	b = a + b;
+	a = b - a;
+	b = b - a;
+	/*int f;
+
+	b = a;
+	a = b;
+	//b = f;*/
+
+	cout << a << endl;
+	cout << b << endl;
+}
