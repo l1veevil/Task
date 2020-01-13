006fe3cca201663072a21c88a08b23d04aa0958d
livevil <55311226+l1veevil@users.noreply.github.com>
2020-01-13 09:54:38 +0300

    Add files via upload

---
+++ zada4a8/zada4a8.cpp
@@ -0,0 +1,49 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	double a, b, r;
+	char z;
+	
+	cout << "Введите операцию" << endl;
+	cin >> a >> z >> b;
+
+	switch (z) {
+
+		case '-': {
+			r = a - b;
+			cout << r;
+			break;
+		}
+		case '+': {
+			r = a + b;
+			cout << r;
+			break;
+		}
+		case '/': {
+			if (b == 0) {
+				cout << "Ошибка";
+			}
+			else {
+				r = a / b;
+				cout << r;
+			}
+			break;
+		}
+		case '*': {
+			r = a * b;
+			cout << r;
+			break;
+		}
+		default:
+			cout << "вы не ввели символ-знак операции";
+	}
+
+
+	}
+
+
+
