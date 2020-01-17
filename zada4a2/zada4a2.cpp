6d5c586b799f72d349b085ac5cf87d0457f7e108
livevil <55311226+l1veevil@users.noreply.github.com>
2020-01-17 14:43:34 +0300

    Add files via upload

---
+++ zada4a2/zada4a2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a = 3, b = 3.14;
+    double c = 3, f = 3.14;
+
+    cout << a << endl << b << endl << c << endl << f;
+
+
+
+}
+
---
+++ zada4a3/zada4a3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+   // int a; double b;
+   // double a; int b;
+    double a, b;
+   // int a, b;
+    cout << "Введите два значения через пробел" << endl;
+    cin >> a >> b;
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+
+
+}
+
